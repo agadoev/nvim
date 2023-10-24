9a9628eb1558f5df40af66a1f043a1cab6a95faa
Алексей Гадоев <gadoevalex@Aleksejs-MacBook-Pro.local>
2023-10-24 12:32:25 +0300

    remove plugins

--- lua/plugins/nvim-commenter.lua
+++
@@ -1,6 +0,0 @@
-
-require('nvim_comment').setup()
-
-vim.api.nvim_set_keymap("", "<leader>/", ":CommentToggle<Cr>", { silent = true })
-vim.api.nvim_set_keymap("", "<leader>v/", ":'<,'>CommentToggle<Cr>", { silent = true })
-
